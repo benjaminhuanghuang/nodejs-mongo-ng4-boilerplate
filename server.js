9a1d084ea61f0527589cede98b4707bccce57ced
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-13 11:41:52 -0700

    server side running

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const app = require("./app");
+const http = require('http');
 
 if (process.env.NODE_ENV === "production") {
 
 }
 
 const PORT = process.env.PORT || 8010; // for prod deployment
-app.listen(PORT, () => {
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
 });
